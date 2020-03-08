f073bb8d9aef79d6348f75dca86b08b7d314e447
gregcarrick <gregcarrick16@gmail.com>
2020-03-08 15:06:48 +0000

    Add ability to save output

---
+++ Orientation.cs
@@ -0,0 +1,18 @@
+﻿namespace MapProjector
+{
+    /// <summary>
+    /// Paper orientation: Landscape or Portrait.
+    /// </summary>
+    public enum Orientation
+    {
+        /// <summary>
+        /// Landscape.
+        /// </summary>
+        Landscape,
+
+        /// <summary>
+        /// Portrait.
+        /// </summary>
+        Portrait,
+    }
+}
